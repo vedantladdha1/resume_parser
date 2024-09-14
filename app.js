d5e938724dfcda4b46d633830397c8a5eab3b3c2
Vedant Laddha <122097882+vedantladdha1@users.noreply.github.com>
2024-09-14 15:06:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ document.getElementById("pdfForm").addEventListener("submit", async function (e)
     formData.append("file", pdfFile);
 
     try {
-      const response = await fetch("http://localhost:3000/test", {
+      const response = await fetch("http://localhost:3000", {
         method: "POST",
         body: formData,
       });
