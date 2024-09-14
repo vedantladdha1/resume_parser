324327c800581f34b6e86df552e2debd9c8bf9cb
Vedant Laddha <122097882+vedantladdha1@users.noreply.github.com>
2024-09-14 15:56:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ document.getElementById("pdfForm").addEventListener("submit", async function (e)
     formData.append("file", pdfFile);
 
     try {
-      const response = await fetch("https://resume-parser-dw4g.onrender.com", {
+      const response = await fetch("https://resume-parser-dw4g.onrender.com/test", {
         method: "POST",
         body: formData,
       });
